b447470cb4c493d6f748c3eee56908f723de06a0
lbw <2328948730@qq.com>
2024-09-09 03:14:03 +0800

    项目初始化

---
+++ LBlogVue/src/router/index.ts
@@ -0,0 +1,23 @@
+import {createRouter,createWebHashHistory,RouteRecordRaw} from 'vue-router'
+
+const routes:RouteRecordRaw[] = [
+    {
+        path:'/',
+        redirect:'/home'
+    },
+    {
+        path:'/home',
+        component: ()=>import('@/pages/home/index.vue')
+    },
+    {
+        path:'/about',
+        component: ()=>import('@/pages/about/index.vue')
+    }
+]
+
+const router = createRouter({
+    history:createWebHashHistory(),
+    routes
+})
+
+export default router
